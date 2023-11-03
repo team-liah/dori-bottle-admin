d519c3186d8333f99ed8488dc9bac32eb1948528
Cluster-Taek <yst811@naver.com>
2023-11-03 17:37:38 +0900

    #6 fix: Quill Editor 이미지 업로드 수정

--- next.config.js
+++ next.config.js
@@ -1,18 +1,19 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-  output: 'standalone',
+  output: "standalone",
   reactStrictMode: true,
   transpilePackages: ["antd"],
   async rewrites() {
     return [
       {
-        source: '/api/:path*',
-        destination: `${
-          process.env.BASE_API_URL || 'https://api.doribottle-id.co.kr'
-        }/admin/api/:path*`,
+        source: "/api/:path*",
+        destination: `${process.env.BASE_API_URL || "https://api.doribottle-id.co.kr"}/admin/api/:path*`,
       },
     ];
+  },
+  images: {
+    domains: ["doribottle-asset.s3.ap-northeast-2.amazonaws.com"],
   },
 };
 
