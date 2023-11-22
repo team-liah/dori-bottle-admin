44c55dbffa660fdb594463553ceedf5da77d36b5
Cluster-Taek <yst811@naver.com>
2023-11-22 16:27:17 +0900

    #6 fix: 모바일 환경에서 input에 Zoom 되는 이슈 수정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -31,6 +31,10 @@ export default function App({ Component, pageProps: { session, ...pageProps } }:
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <link rel="manifest" href="/site.webmanifest" />
+        <meta
+          name="viewport"
+          content="width=device-width, height=device-height, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+        />
       </Head>
       <ConfigProvider
         theme={{
